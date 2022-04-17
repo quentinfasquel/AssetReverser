cd961ccb61ba932297df2ddd1f70e58719881cca
Quentin Fasquel <quentin.fasquel@gmail.com>
2022-04-17 23:36:21 +0200

    Refactoring for SPM support

--- Package.swift
+++ Package.swift
@@ -1,7 +1,21 @@
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "AssetReverser",
-    dependencies : [],
-    exclude: ["Tests"]
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "AssetReverser", targets: ["AssetReverser"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "AssetReverser",
+            exclude: ["Info.plist"]
+        ),
+        .testTarget(
+            name: "AssetReverserTests",
+            exclude: ["Info.plist"]
+        )
+    ]
 )
